ade13be76380ed70424b324aad4669dab9ef2a4f
Expo <crdailey01@gmail.com>
2023-01-23 21:16:46 -0800

    initial ideas

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    // maybe this would be a good one for creating a basic prosthetic hand? although fingers seem pretty similar which doesn't
+//seem to warrant composition as much. ideea to set each digit to a boolean or somertihng for flexion vs extension and havee
+// method cann from a "hand" class that will call all method for all action states of fingers and if the right combo of
+// booleans it will produce 1 of 3 grips: mass flexion, pincer, point. WOuld also want a way to generate a resting state
+// that would look normal
+    //contains section 8 content mostly
+
+}
